f1678b4af4a86e1e44bcd207813821559087f03c
shahr_fdj3g0i <shahriarshahin2024@gmail.com>
2024-06-24 23:41:41 +0600

    live test1

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+void main() {
+  Media media = Media();
+  media.play();
+
+  Song song = Song("Effat");
+  song.play();
+}
+
+class Media {
+  void play() {
+    print("Playing media");
+  }
+}
+
+class Song extends Media {
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist...");
+  }
+}
+
+
